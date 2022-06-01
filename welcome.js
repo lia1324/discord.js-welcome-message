cf1372f12e2254547ee969cf447946134e33756b
lia <96490782+lia1324@users.noreply.github.com>
2022-06-01 18:20:19 +0900

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -13,12 +13,6 @@ client.once('ready', () => {
   console.log(`로그인 완료 | ${client.user.tag}!`)
 })
 
-
-client.on('message', async message => {
-  if (message.content === '핑') return message.reply('퐁')
-  await DokdoHandler.run(message)
-})
-
 client.on('guildMemberAdd', member => {
   member.user.send(`${member.user.username}님! ${member.guild.name}서버에 들어오신걸 환영합니다`).then().catch((error) => { if (error) return })
   const role = button.guild.roles.cache.get("역활 아디")
